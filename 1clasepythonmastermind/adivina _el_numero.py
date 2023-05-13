27047df86c4e3ebf63e5d0bbd1d6830034cd3b51
Vicente Salvo <vicente.salvo@outlook.cl>
2023-05-12 21:44:25 -0400

    Adivina el numero

---
+++ 1clasepythonmastermind/adivina _el_numero.py
@@ -0,0 +1,12 @@
+import random
+
+#Variables
+
+num_secrt = random.randint(1, 5)
+num_user =  int(input("Introduce un numero desde 1 hasta el 5\n"))
+#codigo
+if num_user != num_secrt:
+    print("Has adivinado el numero secrito")
+print("No has adivinado el nuemero pero el numero secreto es {}".format(num_secrt))    
+
+1

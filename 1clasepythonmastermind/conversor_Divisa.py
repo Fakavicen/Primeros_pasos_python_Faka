fbf2659c58612ae21938cea2701a1863058e14c4
Vicente Salvo <vicente.salvo@outlook.cl>
2023-05-13 00:25:06 -0400

    Conversor Divisa Code

--- 1clasepythonmastermind/conversor_Divisa.py
+++ 1clasepythonmastermind/conversor_Divisa.py
@@ -1,2 +1,6 @@
 
-#
+#Variables
+
+price_dolar = 784
+dolar_user = int(input("Introdusca la cantidad de dolar para cambiar\n"))
+print(" {} dolar son {} Pesos Chilenos".format(dolar_user, price_dolar * dolar_user))

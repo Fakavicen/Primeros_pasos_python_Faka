974a8cb0bb8b63a6b8465324e10c473169770320
Vicente Salvo <vicente.salvo@outlook.cl>
2023-05-13 21:49:49 -0400

    juego de historia Code

--- 1clasepythonmastermind/Juego de historia.py
+++ 1clasepythonmastermind/Juego de historia.py
@@ -5,3 +5,31 @@ import random
 tienda = 0
 numeropuerta = random.randint(1,5)
 
+#code
+
+print("Bienvenido al juego \n"
+      "-------------------\n")
+print("Estas en una isla solitaria")
+movimiento1 = input("Â¿Que quieres hacer?\n"
+                   "A - ir a exlorar la isla\n"
+                   "B - Buscar un zona para hacer un copamento\n")
+if movimiento1 == "A":
+     movimiento2 = int(input("Encontraste una puerta con clave\n"
+                    "pero dice que deves adivinar un numero de 1 a 5\n"))
+     if movimiento2 == numeropuerta:
+         print("Ganaste")
+         exit()
+     else:
+       print("Te muriste")
+       print("la clave era {} ".format(numeropuerta))
+if movimiento1 == "B":
+     tienda = input("Te has encontrado una tienda de campaÃ±a\n"
+                    "Â¿que quieres hacer?\n"
+                    "A - tomar la tienda de campaÃ±a y vivir en ella\n"
+                    "B - no tomar la tienda de campaÃ±a\n")
+     if tienda == "B":
+            print("Te muriste por no saber donde dormir")
+            exit()
+     else:
+      print("Has ganado")
+exit()

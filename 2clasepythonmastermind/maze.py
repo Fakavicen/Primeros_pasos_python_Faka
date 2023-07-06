e6dbea4e0529389d613229d428c3449978c386cc
Fakavicen <131844640+Fakavicen@users.noreply.github.com>
2023-07-06 19:52:35 -0400

    Object winner and object deletion

--- 2clasepythonmastermind/maze.py
+++ 2clasepythonmastermind/maze.py
@@ -1,44 +1,55 @@
-import readchar , os
 
-POS_X = 0 
+import os
+import random
+import readchar
+POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
-direction = 0
+NUM_OF_MAP_OBJECTS = 11
+points_earned = 0
+
 my_position = [3, 1]
+map_objects = []
 
-map_objects = [[2,3], [5,4], [3,4], [10,6]]
 
-my_position[POS_X]
-my_position[POS_Y]
+# Generate random objects on the map
+while len(map_objects) < NUM_OF_MAP_OBJECTS:
+    new_position = [random.randint(0, MAP_WIDTH), random.randint(0,MAP_HEIGHT)]
 
+    if new_position not in map_objects:
+        map_objects.append(new_position)
+
+print(map_objects)
+
+
+# Main loop
 while True:
-    # Draw Map
+    # Draw map
     print("+" + "-" * MAP_WIDTH * 3 + "+")
 
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
 
         for coordinate_x in range(MAP_WIDTH):
-
             char_to_draw = " "
 
             for map_object in map_objects:
-                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                if map_object[POS_X] == my_position[POS_X] and map_object[POS_Y] == my_position[POS_Y]:
+                    map_objects.remove(map_object)
+                    points_earned += 1
+                elif map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
                     char_to_draw = "*"
 
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
                 char_to_draw = "@"
 
-
             print(" {} ".format(char_to_draw), end="")
         print("|")
 
-        
     print("+" + "-" * MAP_WIDTH * 3 + "+")
 
-    #movimiento
-    ##direction = input("Donde quieres mover? [WASD]")
+    # Movimiento
     direction = readchar.readchar()
     if direction == "w":
         my_position[POS_Y] -= 1
@@ -52,3 +63,5 @@ while True:
     elif direction == "d":
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
+
+print(points_earned)

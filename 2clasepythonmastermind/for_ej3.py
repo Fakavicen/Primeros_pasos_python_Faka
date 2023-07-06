5d7c57f268d868f99bc9ebd8ea3ff45c89896682
Fakavicen <131844640+Fakavicen@users.noreply.github.com>
2023-07-05 22:24:28 -0400

    exercise 3 of for

---
+++ 2clasepythonmastermind/for_ej3.py
@@ -0,0 +1,10 @@
+import os
+table_number = int(input("Introduce el numero de la tabla que quieres\n"))
+multiplication_number = int(input("Introduce hasta cual tabla quieres que se multiplique\n"))
+
+input("Enter para continuar...\n")
+os.system("cls")
+
+
+for container in range(1, multiplication_number + 1):
+    print("{} x {} = {}".format(table_number, container, (table_number*container)))

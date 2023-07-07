5ce0b37635b0b9a2cb999bf1fdf65b2d174570f6
Fakavicen <131844640+Fakavicen@users.noreply.github.com>
2023-07-06 22:58:57 -0400

    clear code

--- 2clasepythonmastermind/maze.py
+++ 2clasepythonmastermind/maze.py
@@ -1,67 +1,95 @@
-
 import os
 import random
 import readchar
+
+#Variavles
 POS_X = 0
 POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
 NUM_OF_MAP_OBJECTS = 11
-points_earned = 0
 
+tail_length = 0
+tail = []
 my_position = [3, 1]
 map_objects = []
-
-
-# Generate random objects on the map
-while len(map_objects) < NUM_OF_MAP_OBJECTS:
-    new_position = [random.randint(0, MAP_WIDTH), random.randint(0,MAP_HEIGHT)]
-
-    if new_position not in map_objects:
-        map_objects.append(new_position)
-
-print(map_objects)
+life = 1
+end_game = False
 
 
 # Main loop
-while True:
-    # Draw map
-    print("+" + "-" * MAP_WIDTH * 3 + "+")
+while not end_game:
+    # Generate random objects on the map
+    while len(map_objects) < NUM_OF_MAP_OBJECTS:
+        new_position = [random.randint(0, MAP_WIDTH), random.randint(0,MAP_HEIGHT)]
 
+        if new_position not in map_objects and new_position != my_position:
+            map_objects.append(new_position)
+
+    # Draw map
+    os.system("cls")
+    print("+" + "-" * MAP_WIDTH * 3 + "+")
     for coordinate_y in range(MAP_HEIGHT):
         print("|", end="")
 
         for coordinate_x in range(MAP_WIDTH):
+
             char_to_draw = " "
+            objetct_in_cell = None
+            tail_in_cell = None
 
             for map_object in map_objects:
-                if map_object[POS_X] == my_position[POS_X] and map_object[POS_Y] == my_position[POS_Y]:
-                    map_objects.remove(map_object)
-                    points_earned += 1
-                elif map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
+                if map_object[POS_X] == coordinate_x and map_object[POS_Y] == coordinate_y:
                     char_to_draw = "*"
+                    objetct_in_cell = map_object
+
+            for tail_piece in tail:
+                if tail_piece[POS_X] == coordinate_x and tail_piece[POS_Y] == coordinate_y:
+                    char_to_draw = "@"  
+                    tail_in_cell = tail_piece
 
             if my_position[POS_X] == coordinate_x and my_position[POS_Y] == coordinate_y:
                 char_to_draw = "@"
+
+                if objetct_in_cell:
+                        map_objects.remove(objetct_in_cell)
+                        tail_length += 1
+
+                if tail_in_cell:
+                    end_game = True 
 
             print(" {} ".format(char_to_draw), end="")
         print("|")
 
     print("+" + "-" * MAP_WIDTH * 3 + "+")
+    print("Tamaño de cola: {}".format(tail))
 
     # Movimiento
     direction = readchar.readchar()
     if direction == "w":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] -= 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "s":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_Y] += 1
         my_position[POS_Y] %= MAP_HEIGHT
     elif direction == "a":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] -= 1
         my_position[POS_X] %= MAP_WIDTH
     elif direction == "d":
+        tail.insert(0, my_position.copy())
+        tail = tail[:tail_length]
         my_position[POS_X] += 1
         my_position[POS_X] %= MAP_WIDTH
+    elif direction == "q":
+        break
+    os.system("cls")
 
-print(points_earned)
+#EndGame
+os.system("cls")
+print("Has muerto")
